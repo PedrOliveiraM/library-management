f4b0edb11c271084b77f34262788d7b8418ef1f0
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-06 17:48:42 -0300

    feat: create repository and method getAllBooks in LivroService

---
+++ src/main/java/br/com/pedromonteiro/biblioteca/service/LivroService.java
@@ -0,0 +1,20 @@
+package br.com.pedromonteiro.biblioteca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.pedromonteiro.biblioteca.model.LivroEntity;
+import br.com.pedromonteiro.biblioteca.repository.LivroRepository;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository repository;
+
+    public List<LivroEntity> getAllBooks() {
+        return repository.findAll();
+    }
+}
