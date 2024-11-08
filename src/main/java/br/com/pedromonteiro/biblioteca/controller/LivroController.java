42a4cb96e964d22426b21a2dbf67c7850caad9d0
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-08 15:03:34 -0300

    feat: update routes and create update and delete book

--- src/main/java/br/com/pedromonteiro/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/pedromonteiro/biblioteca/controller/LivroController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.pedromonteiro.biblioteca.dto.LivroCreateDto;
 import br.com.pedromonteiro.biblioteca.dto.LivroDto;
 import br.com.pedromonteiro.biblioteca.model.LivroEntity;
 import br.com.pedromonteiro.biblioteca.service.LivroService;
@@ -14,26 +15,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/livros")
+@RequestMapping("/api/livros")
 public class LivroController {
 
     @Autowired
     private LivroService service;
 
-    @GetMapping("")
-    public ResponseEntity<List<LivroEntity>> getAllBooks() {
-        List<LivroEntity> books = this.service.getAllBooks();
-
-        return ResponseEntity.ok(books);
-    }
-
-    @PostMapping("")
-    public ResponseEntity<LivroEntity> createBook(@Valid @RequestBody LivroDto request) {
+    @PostMapping("incluir")
+    public ResponseEntity<LivroEntity> createBook(@Valid @RequestBody LivroCreateDto request) {
         LivroEntity bookEntity = this.service.createBook(request);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -43,4 +40,21 @@ public class LivroController {
         return ResponseEntity.created(location).build();
     }
 
+    @GetMapping("listar")
+    public ResponseEntity<List<LivroEntity>> getAllBooks() {
+        List<LivroEntity> books = this.service.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<LivroEntity> updateBook(@PathVariable Long id, @RequestBody LivroDto request) {
+        LivroEntity updatedBook = this.service.updateBook(id, request);
+        return ResponseEntity.ok().body(updatedBook);
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<LivroEntity> deleteBook(@PathVariable Long id) {
+        ResponseEntity<LivroEntity> removedBook = this.service.removeBook(id);
+        return removedBook;
+    }
 }
--- src/main/java/br/com/pedromonteiro/biblioteca/service/LivroService.java
+++ src/main/java/br/com/pedromonteiro/biblioteca/service/LivroService.java
@@ -3,8 +3,10 @@ package br.com.pedromonteiro.biblioteca.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.pedromonteiro.biblioteca.dto.LivroCreateDto;
 import br.com.pedromonteiro.biblioteca.dto.LivroDto;
 import br.com.pedromonteiro.biblioteca.model.LivroEntity;
 import br.com.pedromonteiro.biblioteca.repository.LivroRepository;
@@ -15,11 +17,7 @@ public class LivroService {
     @Autowired
     private LivroRepository repository;
 
-    public List<LivroEntity> getAllBooks() {
-        return repository.findAll();
-    }
-
-    public LivroEntity createBook(LivroDto dto) {
+    public LivroEntity createBook(LivroCreateDto dto) {
         LivroEntity bookEntity = LivroEntity.builder()
                 .titulo(dto.getTitulo())
                 .isbn(dto.getIsbn())
@@ -29,4 +27,39 @@ public class LivroService {
 
         return repository.save(bookEntity);
     }
+
+    public List<LivroEntity> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public LivroEntity updateBook(Long id, LivroDto dto) {
+        LivroEntity book = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado com id: " + id));
+
+        if (dto.getTitulo() != null) {
+            book.setTitulo(dto.getTitulo());
+        }
+        if (dto.getIsbn() != null) {
+            book.setIsbn(dto.getIsbn());
+        }
+        if (dto.getAutor_id() != null) {
+            book.setAutor_id(dto.getAutor_id());
+        }
+        if (dto.getCategoria_id() != null) {
+            book.setCategoria_id(dto.getCategoria_id());
+        }
+
+        return repository.save(book);
+
+    }
+
+    public ResponseEntity<LivroEntity> removeBook(Long id) {
+        LivroEntity removedBook = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado com id: " + id));
+
+        repository.deleteById(id);
+
+        return ResponseEntity.ok().body(removedBook);
+    }
+
 }
