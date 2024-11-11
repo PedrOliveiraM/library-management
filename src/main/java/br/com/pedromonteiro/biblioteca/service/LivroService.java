f2e7f2e85cdc1ed6fc4136cd315912c179f632b9
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-11 16:16:24 -0300

    fix: add autor and categoria repository for validate id in relations

--- src/main/java/br/com/pedromonteiro/biblioteca/service/LivroService.java
+++ src/main/java/br/com/pedromonteiro/biblioteca/service/LivroService.java
@@ -7,7 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.pedromonteiro.biblioteca.dto.LivroDto;
+import br.com.pedromonteiro.biblioteca.model.AutorEntity;
+import br.com.pedromonteiro.biblioteca.model.CategoriaEntity;
 import br.com.pedromonteiro.biblioteca.model.LivroEntity;
+import br.com.pedromonteiro.biblioteca.repository.AutorRepository;
+import br.com.pedromonteiro.biblioteca.repository.CategoriaRepository;
 import br.com.pedromonteiro.biblioteca.repository.LivroRepository;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -15,23 +19,40 @@ import jakarta.persistence.EntityNotFoundException;
 public class LivroService {
 
     @Autowired
-    private LivroRepository repository;
+    private LivroRepository bookRepository;
+
+    @Autowired
+    private AutorRepository authorRepository;
+
+    @Autowired
+    private CategoriaRepository categoryRepository;
 
     public LivroEntity createBook(LivroDto dto) {
-        LivroEntity bookEntity = LivroEntity.builder()
+        AutorEntity author = authorRepository.findById(dto.getAutor_id())
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Autor não encontrado com ID: " + dto.getAutor_id()));
+
+        CategoriaEntity category = categoryRepository.findById(dto.getCategoria_id())
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Categoria não encontrada com ID: " + dto.getCategoria_id()));
+
+        // 2. Criar a entidade Livro com as informações do DTO
+        LivroEntity livro = LivroEntity.builder()
                 .titulo(dto.getTitulo())
                 .isbn(dto.getIsbn())
+                .autor(author)
+                .categoria(category)
                 .build();
 
-        return repository.save(bookEntity);
+        return bookRepository.save(livro);
     }
 
     public List<LivroEntity> getAllBooks() {
-        return repository.findAll();
+        return bookRepository.findAll();
     }
 
     public LivroEntity updateBook(Long id, LivroDto dto) {
-        LivroEntity book = repository.findById(id)
+        LivroEntity book = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Livro não encontrado com id: " + id));
 
         if (dto.getTitulo() != null) {
@@ -40,19 +61,30 @@ public class LivroService {
         if (dto.getIsbn() != null) {
             book.setIsbn(dto.getIsbn());
         }
-        if (dto.getAutor_id() != null) {
-            book.setLivro(book);
+
+        if (!book.getAutor().getId().equals(dto.getAutor_id())) {
+            AutorEntity autor = authorRepository.findById(dto.getAutor_id())
+                    .orElseThrow(
+                            () -> new IllegalArgumentException("Autor não encontrado com ID: " + dto.getAutor_id()));
+            book.setAutor(autor);
         }
 
-        return repository.save(book);
+        if (!book.getCategoria().getId().equals(dto.getCategoria_id())) {
+            CategoriaEntity categoria = categoryRepository.findById(dto.getCategoria_id())
+                    .orElseThrow(() -> new IllegalArgumentException(
+                            "Categoria não encontrada com ID: " + dto.getCategoria_id()));
+            book.setCategoria(categoria);
+        }
+
+        return bookRepository.save(book);
 
     }
 
     public ResponseEntity<LivroEntity> removeBook(Long id) {
-        LivroEntity removedBook = repository.findById(id)
+        LivroEntity removedBook = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Livro não encontrado com id: " + id));
 
-        repository.deleteById(id);
+        bookRepository.deleteById(id);
 
         return ResponseEntity.ok().body(removedBook);
     }
