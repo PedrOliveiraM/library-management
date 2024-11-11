00f6d970197c02c060e26c1192bd5a37766227ab
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-11 14:36:56 -0300

    refactor: remove imports validations not null

--- src/main/java/br/com/pedromonteiro/biblioteca/dto/AutorDto.java
+++ src/main/java/br/com/pedromonteiro/biblioteca/dto/AutorDto.java
@@ -1,8 +1,6 @@
 package br.com.pedromonteiro.biblioteca.dto;
 
-import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
