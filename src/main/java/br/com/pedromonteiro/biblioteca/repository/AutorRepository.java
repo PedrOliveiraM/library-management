658513d27e1f641ff0b69fc9d5526045d4b47997
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-11 15:08:59 -0300

    feat: create repository for categoria and autor

--- src/main/java/br/com/pedromonteiro/biblioteca/repository/AutorRepository.java
+++ src/main/java/br/com/pedromonteiro/biblioteca/repository/AutorRepository.java
@@ -1,5 +1,9 @@
 package br.com.pedromonteiro.biblioteca.repository;
 
-public class AutorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.pedromonteiro.biblioteca.model.AutorEntity;
+
+public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
 
 }
